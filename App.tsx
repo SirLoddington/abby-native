463e4d738860ffb5b2a312d77e247c69fdd17b4c
SirLoddington <65335429+SirLoddington@users.noreply.github.com>
2023-05-23 16:56:59 +1000

    fonts outsourced

--- App.tsx
+++ App.tsx
@@ -9,28 +9,8 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 import * as SplashScreen from 'expo-splash-screen';
 // import { Georama_400Regular } from '@expo-google-fonts/georama';
-import {
-  useFonts,
-  Poppins_100Thin,
-  Poppins_100Thin_Italic,
-  Poppins_200ExtraLight,
-  Poppins_200ExtraLight_Italic,
-  Poppins_300Light,
-  Poppins_300Light_Italic,
-  Poppins_400Regular,
-  Poppins_400Regular_Italic,
-  Poppins_500Medium,
-  Poppins_500Medium_Italic,
-  Poppins_600SemiBold,
-  Poppins_600SemiBold_Italic,
-  Poppins_700Bold,
-  Poppins_700Bold_Italic,
-  Poppins_800ExtraBold,
-  Poppins_800ExtraBold_Italic,
-  Poppins_900Black,
-  Poppins_900Black_Italic
-} from '@expo-google-fonts/poppins';
-// import { Avinir_400Regular } from '@expo-google-fonts/avinir';
+
+import loadFonts from './loadFonts';
 
 import RootStack from './application/RootStack';
 
@@ -42,26 +22,7 @@ export default function App() {
   // // Initialize state for app readiness
   // const [appIsReady, setAppIsReady] = useState(false);
 
-  const [fontsLoaded] = useFonts({
-    Poppins_100Thin,
-    Poppins_100Thin_Italic,
-    Poppins_200ExtraLight,
-    Poppins_200ExtraLight_Italic,
-    Poppins_300Light,
-    Poppins_300Light_Italic,
-    Poppins_400Regular,
-    Poppins_400Regular_Italic,
-    Poppins_500Medium,
-    Poppins_500Medium_Italic,
-    Poppins_600SemiBold,
-    Poppins_600SemiBold_Italic,
-    Poppins_700Bold,
-    Poppins_700Bold_Italic,
-    Poppins_800ExtraBold,
-    Poppins_800ExtraBold_Italic,
-    Poppins_900Black,
-    Poppins_900Black_Italic
-  });
+  const fontsLoaded = loadFonts();
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
